733e5f61fdec09ca0291d818ca4bf92b8e76b33f
muhammadardie <ardiemuhammad3@gmail.com>
2020-09-09 17:05:37 +0700

    add nodeintegration

--- main.js
+++ main.js
@@ -59,7 +59,13 @@ function sendStatusToWindow(text) {
   win.webContents.send('message', text);
 }
 function createDefaultWindow() {
-  win = new BrowserWindow();
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  });
   win.webContents.openDevTools();
   win.on('closed', () => {
     win = null;
